56a43ae479563c9dd30b897953a4df314b1fb629
Николай Курко <nikolajkurko@MacBook-Air-Nikolaj.local>
2020-03-23 05:36:57 +0300

    added searchDentists clinic method

--- OOP_2_3/SourceCode/Interfaces/ClinicMethods/SearchMethods.cpp
+++ OOP_2_3/SourceCode/Interfaces/ClinicMethods/SearchMethods.cpp
@@ -35,8 +35,79 @@ void Clinic::searchDoctors() const {
 }
 
 void Clinic::searchDentists() const {
-    
+    int selection;
+    string name;
+    string surname;
+    int age;
+    string education;
+    int experience;
+    float salary;
+    while (true) {
+        cout << "\n1 - Поиск по имени\n2 - Поиск по фамилии\n3 - Поиск по возрасту" <<
+        "\n4 - Поиск по образованию\n5 - Поиск по опыту работы\n6 - Поиск по зарплате\n7 - Назад" << endl;
+        cout << "Выберите критерий поиска: ";
+        selection = CorrectCin<int>();
+        switch (selection) {
+            case 1:
+                cout << "Введите имя: ";
+                getline(cin, name);
+                for (auto& dentist: dentists) {
+                    if (dentist.getName() == name)
+                        dentist.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 2:
+                cout << "Введите фамилию: ";
+                getline(cin, surname);
+                for (auto& dentist: dentists) {
+                    if (dentist.getSurname() == surname)
+                        dentist.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 3:
+                cout << "Введите возраст: ";
+                age = CorrectCin<int>();
+                for (auto& dentist: dentists) {
+                    if (dentist.getAge() == age)
+                        dentist.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 4:
+                cout << "Введите образованиие: ";
+                getline(cin, education);
+                for (auto& dentist: dentists) {
+                    if (dentist.getEducation() == education)
+                        dentist.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 5:
+                cout << "Введите опыт работы: ";
+                experience = CorrectCin<int>();
+                for (auto& dentist: dentists) {
+                    if (dentist.getExperince() == experience)
+                        dentist.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 6:
+                cout << "Введите зарплату: ";
+                salary = CorrectCin<float>();
+                for (auto& dentist: dentists) {
+                    if (dentist.getSalary() == salary)
+                        dentist.printInformation();
+                    cout << "\n";
+                }
+                break;
+            default:
+                return;
+        }
+    }
 }
+
 void Clinic::searchParamedics() const {
     int selection;
     string name;
