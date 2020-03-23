848d8c64e14e52b40a5d8f9402b93a6f58cf8d9c
Николай Курко <nikolajkurko@MacBook-Air-Nikolaj.local>
2020-03-23 07:30:09 +0300

    editDentist function implemented

--- OOP_2_3/SourceCode/Interfaces/Menu.cpp
+++ OOP_2_3/SourceCode/Interfaces/Menu.cpp
@@ -431,7 +431,52 @@ void editDoctor(Doctor& doctor) {
 }
 
 void editDentist(Dentist& dentist) {
-    
+    int selection;
+    string name;
+    string surname;
+    int age;
+    string education;
+    int experience;
+    float salary;
+    while (true) {
+        cout << "\n1 - Редактировать имя\n2 - Редактировать фамилию\n3 - Редактировать возраст" <<
+        "\n4 - Редактировать образование\n5 - Редактировать опыт работы\n6 - Редактировать зарплату\n7 - Назад" << endl;
+        cout << "Выберите пункт меню: ";
+        selection = CorrectCin<int>();
+        switch (selection) {
+            case 1:
+                cout << "Введите новое значение имени: ";
+                getline(cin, name);
+                dentist.setName(name);
+                break;
+            case 2:
+                cout << "Введите новое значение фамилии: ";
+                getline(cin, surname);
+                dentist.setSurname(surname);
+                break;
+            case 3:
+                cout << "Введите новое значение возраста: ";
+                age = CorrectCin<int>();
+                dentist.setAge(age);
+                break;
+            case 4:
+                cout << "Введите новое значение образования: ";
+                getline(cin, education);
+                dentist.setEducation(education);
+                break;
+            case 5:
+                cout << "Введите новое значение опыта работы: ";
+                experience = CorrectCin<int>();
+                dentist.setExperience(experience);
+                break;
+            case 6:
+                cout << "Введите новое значение зарплаты: ";
+                salary = CorrectCin<float>();
+                break;
+            default:
+                return;
+        }
+    }
 }
 
 void editParamedic(Paramedic& paramedic) {
