be78907b129d4bac7479e559dbba0aafdccb311d
Николай Курко <nikolajkurko@MacBook-Air-Nikolaj.local>
2020-03-23 05:16:16 +0300

    added searchPatients clinic method

--- OOP_2_3/SourceCode/Interfaces/ClinicMethods/SearchMethods.cpp
+++ OOP_2_3/SourceCode/Interfaces/ClinicMethods/SearchMethods.cpp
@@ -44,5 +44,44 @@ void Clinic::searchNurses() const {
     
 }
 void Clinic::searchPatients() const {
-    
+    int selection;
+    string name;
+    string surname;
+    int age;
+    while (true) {
+        cout << "\n1 - Поиск по имени\n2 - Поиск по фамилии\n3 - Поиск по возрасту\n4 - Назад" << endl;
+        cout << "Выберите критерий поиска: ";
+        selection = CorrectCin<int>();
+        switch (selection) {
+            case 1:
+                cout << "Введите имя: ";
+                getline(cin, name);
+                for (auto& patient: patients) {
+                    if (patient->getName() == surname)
+                        patient->printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 2:
+                cout << "Введите фамилию: ";
+                getline(cin, surname);
+                for (auto& patient: patients) {
+                    if (patient->getSurname() == surname)
+                        patient->printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 3:
+                cout << "Введите возраст: ";
+                age = CorrectCin<int>();
+                for (auto& patient: patients) {
+                    if (patient->getAge() == age)
+                        patient->printInformation();
+                    cout << "\n";
+                }
+                break;
+            default:
+                return;
+        }
+    }
 }
