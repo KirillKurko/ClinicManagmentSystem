005c9b6486b3a26e22620c32b1f0bf39d5e31e50
Николай Курко <nikolajkurko@MacBook-Air-Nikolaj.local>
2020-03-23 05:32:10 +0300

    added searchParamedics clinic method

--- OOP_2_3/SourceCode/Interfaces/ClinicMethods/SearchMethods.cpp
+++ OOP_2_3/SourceCode/Interfaces/ClinicMethods/SearchMethods.cpp
@@ -38,7 +38,88 @@ void Clinic::searchDentists() const {
     
 }
 void Clinic::searchParamedics() const {
-    
+    int selection;
+    string name;
+    string surname;
+    int age;
+    string education;
+    int experience;
+    float salary;
+    string region;
+    while (true) {
+        cout << "\n1 - Поиск по имени\n2 - Поиск по фамилии\n3 - Поиск по возрасту" <<
+        "\n4 - Поиск по образованию\n5 - Поиск по опыту работы\n6 - Поиск по зарплате"
+        << "\n7 - Поиск по закрепленному региону\n8 - Назад" << endl;
+        cout << "Выберите критерий поиска: ";
+        selection = CorrectCin<int>();
+        switch (selection) {
+            case 1:
+                cout << "Введите имя: ";
+                getline(cin, name);
+                for (auto& paramedic: paramedics) {
+                    if (paramedic.getName() == name)
+                        paramedic.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 2:
+                cout << "Введите фамилию: ";
+                getline(cin, surname);
+                for (auto& paramedic: paramedics) {
+                    if (paramedic.getSurname() == surname)
+                        paramedic.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 3:
+                cout << "Введите возраст: ";
+                age = CorrectCin<int>();
+                for (auto& paramedic: paramedics) {
+                    if (paramedic.getAge() == age)
+                        paramedic.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 4:
+                cout << "Введите образованиие: ";
+                getline(cin, education);
+                for (auto& paramedic: paramedics) {
+                    if (paramedic.getEducation() == education)
+                        paramedic.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 5:
+                cout << "Введите опыт работы: ";
+                experience = CorrectCin<int>();
+                for (auto& paramedic: paramedics) {
+                    if (paramedic.getExperince() == experience)
+                        paramedic.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 6:
+                cout << "Введите зарплату: ";
+                salary = CorrectCin<float>();
+                for (auto& paramedic: paramedics) {
+                    if (paramedic.getSalary() == salary)
+                        paramedic.printInformation();
+                    cout << "\n";
+                }
+                break;
+            case 7:
+                cout << "Введите закрепленный регион: ";
+                getline(cin, region);
+                for (auto& paramedic: paramedics) {
+                    if (paramedic.getRegion() == region)
+                        paramedic.printInformation();
+                    cout << "\n";
+                }
+                break;
+            default:
+                return;
+        }
+    }
 }
 
 void Clinic::searchNurses() const {
